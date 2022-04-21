1aa283f470601202a573c57a31d5420630d759b2
Kazuming <kimukazu.feb@gmail.com>
2022-04-21 18:35:46 +0900

    W04

---
+++ W04/create_csv.py
@@ -0,0 +1,14 @@
+import random
+colors = ["red", "orange", "pink", "snow", "yellow", "green", "lime", "cyan","blue","purple"]
+def create_csv(n):
+    data = "x,y,r,color\n"
+    for _ in range(n):
+        x = random.randrange(10, 990, 10)
+        y = random.randrange(10, 990, 10)
+        r = random.randrange(10, 30, 2)
+        color = colors[random.randrange(len(colors))]
+        data += f"{x},{y},{r},{color}\n"
+    f = open(f"w04_task1.csv", 'w')
+    f.write(data)
+
+create_csv(100)
